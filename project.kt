ef2a1f36ce5e94808a79371ccb81010101b9cce7
Shivam Kumar <shivamk1717@gmail.com>
2020-05-29 14:43:55 +0530

    done stage 2 works basic calculation in kotlin

--- project.kt
+++ project.kt
@@ -1,11 +1,15 @@
 package machine
 
 fun main() {
-    println("Starting to make a coffee")
-    println("Grinding coffee beans")
-    println("Boiling water")
-    println("Mixing boiled water with crushed coffee beans")
-    println("Pouring coffee into the cup")
-    println("Pouring some milk into the cup")
-    println("Coffee is ready!")
+    println("Write how many cups of coffee you will need: > ")
+    var cups_needed=readLine()!!.toInt()
+    print("For ")
+    print(cups_needed)
+    println("cups of coffee you will need:")
+    print(cups_needed*200)
+    println(" ml of water")
+    print(cups_needed*50)
+    println(" ml of milk")
+    print(cups_needed*15)
+    println(" g of coffee beans")
 }
